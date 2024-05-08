2b71598e06bf15c024eff812967a16b71a60d971
Marcus <marcus-v@outlook.com>
2024-05-08 19:29:22 -0300

    create view model factory

---
+++ app/src/main/java/com/example/hqshop/viewmodels/factory/BaseCategoryViewModelFactory.kt
@@ -0,0 +1,18 @@
+package com.example.hqshop.viewmodels.factory
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.hqshop.models.CategoryModel
+import com.example.hqshop.viewmodels.CategoryViewModel
+import com.google.firebase.firestore.FirebaseFirestore
+
+class BaseCategoryViewModelFactory(
+    private var firestore: FirebaseFirestore,
+    private var category: CategoryModel
+) : ViewModelProvider.AndroidViewModelFactory(){
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return CategoryViewModel(firestore, category) as T
+    }
+
+}
--- app/src/main/java/com/example/hqshop/views/fragments/category/MarvelCategoryFragment.kt
+++ app/src/main/java/com/example/hqshop/views/fragments/category/MarvelCategoryFragment.kt
@@ -1,4 +1,10 @@
 package com.example.hqshop.views.fragments.category
 
+import android.os.Bundle
+import android.view.View
+
 class MarvelCategoryFragment: BaseCategoryFragment()  {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
